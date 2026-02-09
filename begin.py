f742f5ce8378552a2cefb396a96b5a3f98ae4993
Han <nguyenthanhngochan14122589@gmail.com>
2026-02-09 18:30:31 +0700

    data

--- begin.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-data= [100,102,104]
-
-series= pd.Series (data)
-
-print(series)
---
+++ series.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+
+# data= [100,102,104,200,202]
+
+# series= pd.Series (data, index=["apartment #1","apartmet #2","apartment #3","apartment #4"])
+
+# series.loc["apartment #1"] = 101
+
+# # print(series.loc["apartment #1"])
+
+# # print(series.iloc[0])
+
+# # loc[] là location 
+
+# print(series [series < 200 ])
+
+# calories={"day1":1750, "day2":2100,"day3":1700}
+
+# series = pd.Series (calories)
+
+# print(series.loc["day1"])
+
+# series.iloc[0]+=500
+
+# print(series.loc["day1"])
+
+# print(series[series <2000])
+
+data ={"Name": ["Spongebob","Patrick", "Squidward"]}
